17da8e62b6dc71a448d6c0cb977b2dfadd6a886a
Roberto Silva <roberto.silva@arcasolutions.com>
2013-10-18 15:13:33 -0300

    Applies generic module to classified result

--- eDirectory/src/main/java/com/arcasolutions/ui/activity/classified/ClassifiedResultActivity.java
+++ eDirectory/src/main/java/com/arcasolutions/ui/activity/classified/ClassifiedResultActivity.java
@@ -2,95 +2,37 @@ package com.arcasolutions.ui.activity.classified;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.widget.AdapterView;
 
-import com.androidquery.AQuery;
 import com.arcasolutions.R;
-import com.arcasolutions.api.Client;
-import com.arcasolutions.api.constant.SearchBy;
 import com.arcasolutions.api.model.BaseCategory;
-import com.arcasolutions.api.model.Classified;
 import com.arcasolutions.api.model.ClassifiedResult;
-import com.arcasolutions.ui.adapter.ModuleResultAdapter;
-import com.google.common.collect.Lists;
+import com.arcasolutions.api.model.Module;
+import com.arcasolutions.ui.activity.BaseActivity;
+import com.arcasolutions.ui.fragment.ModuleResultFragment;
 
-import java.util.ArrayList;
-import java.util.List;
+public class ClassifiedResultActivity extends BaseActivity implements ModuleResultFragment.OnModuleSelectionListener {
 
-public class ClassifiedResultActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
-
-    public static final String EXTRA_ITEMS = "items";
     public static final String EXTRA_CATEGORY = "category";
-
-    private BaseCategory mCategory;
-    private int mPage;
-
-    private final List<Classified> mClassifieds = Lists.newArrayList();
-
-    private ModuleResultAdapter<Classified> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.custom_list_view);
 
+        BaseCategory category = getIntent().getParcelableExtra(EXTRA_CATEGORY);
 
-        mAdapter = new ModuleResultAdapter<Classified>(this, mClassifieds);
-
-        AQuery aq = new AQuery(this);
-        aq.id(android.R.id.list)
-                .itemClicked(this)
-                .adapter(mAdapter);
-
-        ArrayList<Parcelable> items = getIntent().getParcelableArrayListExtra(EXTRA_ITEMS);
-        if (items != null) {
-            for (Parcelable p : items) {
-                if (p instanceof Classified) {
-                    mClassifieds.add((Classified) p);
-                }
-            }
-            mAdapter.notifyDataSetChanged();
-        }
-
-        mCategory = getIntent().getParcelableExtra(EXTRA_CATEGORY);
-        loadClassifieds();
-
-    }
-
-    private void loadClassifieds() {
-
-        Client.Builder builder = new Client.Builder(ClassifiedResult.class);
-        builder.page(mPage);
-        if (mCategory != null) {
-            builder.categoryId(mCategory.getId())
-                    .searchBy(SearchBy.CATEGORY);
-        }
-        builder.execAsync(new Client.RestListener<ClassifiedResult>() {
-
-            @Override
-            public void onComplete(ClassifiedResult result) {
-                List<Classified> classifieds = result.getResults();
-                if (classifieds != null) {
-                    mClassifieds.addAll(classifieds);
-                    mAdapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onFail(Exception ex) {
-                ex.printStackTrace();
-            }
-        });
+        ModuleResultFragment<ClassifiedResult> fragment = ModuleResultFragment.newInstance(ClassifiedResult.class, category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_content, fragment)
+                .commit();
 
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        Intent intent = new Intent(this, ClassifiedDetailActivity.class);
-        intent.putExtra(ClassifiedDetailActivity.EXTRA_ID, l);
-        startActivity(intent);
+    public void onModuleSelected(Module module, int position, long id) {
+        if (module != null) {
+            Intent intent = new Intent(this, ClassifiedDetailActivity.class);
+            intent.putExtra(ClassifiedDetailActivity.EXTRA_ID, module.getId());
+            startActivity(intent);
+        }
     }
 }
