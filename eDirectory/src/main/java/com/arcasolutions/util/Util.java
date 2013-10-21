93f8c6ee3c432f0462cb5a1f9e505f6fdc19fb31
Roberto Silva <roberto.silva@arcasolutions.com>
2013-10-21 09:07:17 -0200

    Adds method to calculate distance from me

--- eDirectory/src/main/java/com/arcasolutions/util/Util.java
+++ eDirectory/src/main/java/com/arcasolutions/util/Util.java
@@ -1,11 +1,27 @@
 package com.arcasolutions.util;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 
 public class Util {
 
+    private static Location mMyLocation;
+
     public static boolean hasHoneycomb() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+    }
+
+    public static double distanceFromMe(double latitude, double longitude) {
+        if (mMyLocation == null) return -1;
+        Location l = new Location(LocationManager.PASSIVE_PROVIDER);
+        l.setLatitude(latitude);
+        l.setLongitude(longitude);
+        return mMyLocation.distanceTo(l);
+    }
+
+    public static void setMyLocation(Location myLocation) {
+        mMyLocation = myLocation;
     }
 
 }
