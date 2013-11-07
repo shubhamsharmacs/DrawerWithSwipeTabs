3535b928f8ab6f07819dba67b3b46509e5959a71
Roberto Silva <roberto.silva@arcasolutions.com>
2013-11-07 09:45:54 -0200

    Requests indeterminate progress to window

--- eDirectory/src/main/java/com/arcasolutions/ui/activity/BaseActivity.java
+++ eDirectory/src/main/java/com/arcasolutions/ui/activity/BaseActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -102,6 +103,9 @@ public abstract class BaseActivity extends ActionBarActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
         setContentView(R.layout.activity_main);
 
         APPTAG = getString(R.string.app_name);
